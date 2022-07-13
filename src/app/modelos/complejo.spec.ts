9bfa47cc11463973c7b8eb87bf87f7e10e35ef9e
tomi-g <tomigarci19@outlook.com>
2022-07-13 18:41:48 -0300

    Carpetas Modelos y Servicios

---
+++ src/app/modelos/complejo.spec.ts
@@ -0,0 +1,7 @@
+import { Complejo } from './complejo';
+
+describe('Complejo', () => {
+  it('should create an instance', () => {
+    expect(new Complejo()).toBeTruthy();
+  });
+});
---
+++ src/app/modelos/complejo.ts
@@ -0,0 +1,2 @@
+export class Complejo {
+}
