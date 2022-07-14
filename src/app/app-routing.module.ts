9f8547d10f290f20a9355a501303ee1905621196
tomi-g <tomigarci19@outlook.com>
2022-07-13 23:29:49 -0300

    Subo Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IndexComponent } from './componentes/landing/index/index.component';
-import { LandingModule } from './componentes/landing/landing.module';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./componentes/landing/landing.module').then(m => m.LandingModule) },
-  // { path: 'auth', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)  },
+  { path: 'auth', loadChildren: () => import('./componentes/account/account.module').then(m => m.AccountModule)  },
   // { path: 'pages', loadChildren: () => import('./extraspages/extraspages.module').then(m => m.ExtraspagesModule), canActivate: [AuthGuard] },
   // { path: 'landing', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)},
 ];
---
+++ src/app/componentes/account/account.module.ts
@@ -0,0 +1,32 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+// Load Icons
+import { defineLordIconElement } from 'lord-icon-element';
+import lottie from 'lottie-web';
+
+import { AccountRoutingModule } from './account-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegistrarseComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AccountRoutingModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AccountModule { 
+  constructor() {
+    defineLordIconElement(lottie.loadAnimation);
+  }
+}
