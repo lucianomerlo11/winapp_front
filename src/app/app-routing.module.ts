02bfb4641106f7c7f38ee38f1ee6827484acccd2
tomi-g <tomigarci19@outlook.com>
2022-07-15 16:54:28 -0300

    Ruteo de pantallas en el inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import { LayoutComponent } from './componentes/layout/layout.component';
 
 
 const routes: Routes = [
-  { path: '', component: LayoutComponent/*, loadChildren: () => import('./componentes/landing/landing.module').then(m => m.LandingModule)*/ },
+  { path: '', component: LayoutComponent, loadChildren: () => import('./componentes/pantallas/pantallas.module').then(m => m.PantallasModule) },
   { path: 'auth', loadChildren: () => import('./componentes/account/account.module').then(m => m.AccountModule)  },
   // { path: 'pages', loadChildren: () => import('./extraspages/extraspages.module').then(m => m.ExtraspagesModule), canActivate: [AuthGuard] },
   // { path: 'landing', loadChildren: () => import('./componentes/landing/landing.module').then(m => m.LandingModule) },
   { path: '', loadChildren: () => import('./componentes/landing/landing.module').then(m => m.LandingModule) },
   
   // { path: 'auth', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)  },
-  { path: 'ag', loadChildren: () => import('./componentes/pantallas/pantallas.module').then(m => m.PantallasModule)},
+  // { path: 'ag', loadChildren: () => import('./componentes/pantallas/pantallas.module').then(m => m.PantallasModule)},
   // { path: 'landing', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)},
 ];
 
